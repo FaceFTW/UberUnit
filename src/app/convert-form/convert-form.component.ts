2e4b71a766ad816655c9c89cb38f84b47501eaed
Alex Westerman <westeraj@rose-hulman.edu>
2021-07-25 16:55:49 -0400

    Beginning to implement Unit Conversion
    -Added Angular Forms Dependency

--- src/app/convert-form/convert-form.component.ts
+++ src/app/convert-form/convert-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-convert-form',
   templateUrl: './convert-form.component.html',
   styleUrls: ['./convert-form.component.css']
 })
 export class ConvertFormComponent implements OnInit {
+  input1 = new FormControl('');
+  input2 = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  doConvert(){};
+  swapVals(){};
 }
